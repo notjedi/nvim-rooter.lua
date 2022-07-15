2508b6c1be97e7b357d38e3fe3704a5d82408053
Avimitin <avimitin@gmail.com>
2022-07-15 19:40:03 +0800

    Fix manual trigger logic (#9)
    Co-authored-by: Krithic Kumar <krithickumarub@protonmail.com>

--- plugin/nvim-rooter.lua
+++ plugin/nvim-rooter.lua
@@ -1,4 +1,4 @@
-if vim.fn.exists('g:loaded_nvim_rooter') == 1 or vim.fn.has('nvim-0.5') == 0 then
+if vim.fn.exists('g:loaded_nvim_rooter') == 1 or not vim.fn.has('nvim-0.7') then
   return
 end
 vim.g.loaded_nvim_rooter = 1
@@ -7,9 +7,4 @@ vim.cmd([[
   command! Rooter lua require 'nvim-rooter'.rooter()
   command! RooterToggle lua require 'nvim-rooter'.rooter_toggle()
   command! GetRootDir lua require 'nvim-rooter'.get_root()
-  augroup nvim_rooter
-    autocmd!
-    autocmd BufRead * lua vim.api.nvim_buf_set_var(0, 'root_dir', nil)
-    autocmd BufEnter * nested lua require'nvim-rooter'.rooter()
-  augroup END
 ]])
